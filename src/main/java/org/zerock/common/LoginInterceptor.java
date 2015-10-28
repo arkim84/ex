a7591aaf685ef719098e07b707a12ddcac4e8cc2
프람트 김아름 <arkim84@prompt.co.kr>
2015-10-28 10:48:42 +0900

    인터셉터(테스트 완료)

--- src/main/java/org/zerock/common/LoginInterceptor.java
+++ src/main/java/org/zerock/common/LoginInterceptor.java
@@ -3,11 +3,7 @@ package org.zerock.common;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.zerock.common.annotation.LoginCheck;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter{
 	
@@ -15,14 +11,9 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
 	
-		System.out.println("test");
-		LoginCheck usingAuth = ((HandlerMethod) handler).getMethodAnnotation(LoginCheck.class);
-		if(usingAuth != null){
-			
-		}
+		System.out.println("preHandle call..");
 		
 		return true;
-		
 	}
 	
 }
