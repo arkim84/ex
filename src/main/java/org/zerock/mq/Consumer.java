1f7080f57c7f991240d5a993c853072a4c6346fe
프람트 김아름 <arkim84@prompt.co.kr>
2015-11-11 15:53:22 +0900

    mq 관련 설정 수정

--- src/main/java/org/zerock/mq/Consumer.java
+++ src/main/java/org/zerock/mq/Consumer.java
@@ -2,9 +2,9 @@ package org.zerock.mq;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class Consumer {
 	
 	private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
--- src/main/java/org/zerock/mq/Producer.java
+++ src/main/java/org/zerock/mq/Producer.java
@@ -2,9 +2,9 @@ package org.zerock.mq;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class Producer {
 
 	@Autowired
