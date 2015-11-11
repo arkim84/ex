d8e2e2421bfa11db9638403bf8dbae801064a076
프람트 김아름 <arkim84@prompt.co.kr>
2015-11-11 10:56:43 +0900

    메세지, 포인트 객체 생성

---
+++ src/main/java/org/zerock/persistence/MessageDAOImpl.java
@@ -0,0 +1,30 @@
+package org.zerock.persistence;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.zerock.domain.MessageVO;
+
+public class MessageDAOImpl implements MessageDAO{
+	
+	@Inject
+	private SqlSession session;
+	
+	private static String namespace = "org.zerock.mapper.MessageMapper";
+	
+	@Override
+	public void create(MessageVO vo) throws Exception{
+		session.insert(namespace+".create", vo);
+	}
+	
+	@Override
+	public MessageVO readMessage(Integer mid) throws Exception{
+		return session.selectOne(namespace+".readMessage", mid);
+	}
+	
+	@Override
+	public void updateState(Integer mid) throws Exception{
+		session.update(namespace+".updateState", mid);
+	}
+
+}
